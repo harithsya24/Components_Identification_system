63114311e3d9699fd40d8d740919f607b4b310fb
Amrutha_Kanakatte_Ravishankar <amruthaputti01@gmail.com>
2026-02-21 21:56:27 -0500

    Model Training Done

--- componentsDetection/constant/training_pipeline/__init__.py
+++ componentsDetection/constant/training_pipeline/__init__.py
@@ -11,3 +11,11 @@ DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_STATUS_FILE: str = "status.txt"
 
 DATA_VALIDATION_ALL_REQUIRED_FILE = ["train", "validation", "data.yaml"]
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 3
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
--- componentsDetection/pipeline/training_pipeline.py
+++ componentsDetection/pipeline/training_pipeline.py
@@ -3,14 +3,15 @@ from componentsDetection.logger import logging
 from componentsDetection.exception import AppException
 from componentsDetection.components.data_ingestion import DataIngestion
 from componentsDetection.components.data_validation import DataValidation
-from componentsDetection.entity.config_entity import DataIngestionConfig, DataValidationConfig
-from componentsDetection.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact
+from componentsDetection.entity.config_entity import DataIngestionConfig, DataValidationConfig, ModelTrainerConfig
+from componentsDetection.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact, ModelTrainerArtifact
+from componentsDetection.components.model_trainer import ModelTrainer
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        # self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
         
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
@@ -57,7 +58,19 @@ class TrainPipeline:
 
         except Exception as e:
             raise AppException(e, sys) from e
+        
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
 
+        except Exception as e:
+            raise AppException(e, sys)
+        
 
     def run_pipeline(self) -> None:
         try:
@@ -66,10 +79,10 @@ class TrainPipeline:
                 data_ingestion_artifact=data_ingestion_artifact
             )
 
-            # if data_validation_artifact.validation_status == True:
-            #     model_trainer_artifact = self.start_model_trainer()
-            # else:
-            #     raise Exception("Your data is not in correct format")
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            else:
+                raise Exception("Your data is not in correct format")
         
         except Exception as e:
             raise AppException(e, sys)
